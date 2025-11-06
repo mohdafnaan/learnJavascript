be2fa0302654a4ea75a3a6247ff57489f9fed0fe
mohdafnaan <mohdafnaan833@gmail.com>
2025-11-06 12:14:41 +0000

    file of functions

---
+++ 10.js
@@ -0,0 +1,23 @@
+// how a function is performed im the backend,
+// var n = 2 ;
+function square(num) {
+  ans = num * num;
+  return ans;
+}
+var square2 = square(2);
+console.log(square2);
+
+var squaere4 = square(4);
+console.log(squaere4);
+
+//hoisting 
+
+// if you print a variable before declaring it..gives not defined.
+
+let x = 12;
+console.log(x); // it gives the output
+
+
+var x = 10;
+a();
+b();
